668553ec64d48793dc23da066552d379aa658187
Lucas Felipe <lkzeron@gmail.com>
2023-08-10 02:43:10 -0300

    fix: remove bug  git duplicate

--- anotaçoes_aula/teste/mock.md
+++ anotaçoes_aula/teste/mock.md
@@ -1,1 +1,10 @@
 <h1 align="center"> Mock service  work</h1>
+
+<h2>Utilizameremos biblioteca:</h2>
+
+<p>
+Mock service work uma da mais utilizada pelo professor luiz miranda, <a href="https://mswjs.io/docs/getting-started/install"> link </a>
+<p>
+
+<li> Comando para install: npm i msw --save-dev 
+
--- aula_react/.prettierrc.js
+++ aula_react/.prettierrc.js
@@ -1,19 +1,7 @@
-<<<<<<< Updated upstream
-EditorConfig is awesome: https://EditorConfig.org
-
 module.exports = {
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
   printWidth: 120,
-  tabWidth: 2
-}
-=======
-module.exports = {
-  semi: true,
-  trailingComma:'all',
-  singleQuote:true,
-  printWidth:120,
   taWidth: 2
 }
->>>>>>> Stashed changes
