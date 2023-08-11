c1b817cfe336a6e552ed23536c6b8537d0829dc3
Lucas Felipe <lkzeron@gmail.com>
2023-08-11 13:24:51 -0300

    feat: utilização do useRef com Dom

---
+++ intro_hook/src/components/HookUserRef.js
@@ -0,0 +1,51 @@
+import { React, useRef, useState, useEffect } from 'react'
+
+const HookUserRef = () => {
+  // 1 - useRef
+  //vou criar variavel de referencia valores
+  const numberRef = useRef(0);
+  const [counter, setCounter] = useState(0);
+  const [counterB, setCounterB] = useState(0);
+
+  useEffect(() => {
+    numberRef.current = numberRef.current + 1
+  });
+  // 2 - useRef Com DOM
+  const inputRef = useRef()
+  const [text, setText] = useState("useRef Com Dom")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("")
+
+    inputRef.current.focus()
+  }
+
+  return (
+    <div>
+      <h2>
+        HookUserRef
+      </h2>
+      <p>
+        O componente renderizou : {numberRef.current} vezes
+      </p>
+      <p>
+        counter A {counter}
+      </p>
+      <button onClick={() => setCounter(counter + 1)}>Contador A</button>
+      <p>
+        counter B {counterB}
+      </p>
+      <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+      <hr />
+      {/* useRef com DOM */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} value={text} onChange={(e) => { setText(e.target.value) }} />
+        <input type="submit" value="Enviar" />
+      </form>
+      <hr />
+    </div>
+  )
+}
+
+export default HookUserRef;
