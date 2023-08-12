18b08e30180bb574d4388eff3ca5498cac094dfc
Lucas Felipe <lkzeron@gmail.com>
2023-08-12 20:25:17 -0300

    feat: update

--- mini_projetos_react/list-js-task/src/components/main.js
+++ mini_projetos_react/list-js-task/src/components/main.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-
+import './Main.css'
 export default class Main extends Component {
   state = {
-    novaTask: '',
+    newTask: '',
   };
-  input = (e) => {
+
+  handleInput = (e) => {
     this.setState({
       newTask: e.target.value,
-    })
+    });
   }
 
 
   render() {
-    const { novaTask } = this.state;
-    return (
+    const { newTask } = this.state;
 
+    return (
       <div className="main">
         <h1>Lista de Tarefa com Js</h1>
         <form>
-          <input onCahnge={this.inputMudou} type="text" />
-          <p>{novaTask}</p>
+          <input onChange={this.handleInput} type="text" />
           <button tpe="submit"> Adcionar tarefa</button>
+          <p>{newTask}</p>
         </form>
       </div>)
   }
