9595684831b24e6bf2745f172e2a5af45d87ee7c
Lucas Felipe <lkzeron@gmail.com>
2023-08-14 20:49:56 -0300

    feat: update logo estilizacao

--- mini_projetos_react/Cadastro_user/frontend/system_crud_user/src/components/templates/logo/Logo.jsx
+++ mini_projetos_react/Cadastro_user/frontend/system_crud_user/src/components/templates/logo/Logo.jsx
@@ -1,7 +1,10 @@
-import './Logo.css';
+import logo from '../../../assets/logo/logo.png'
 import React from 'react';
+import './Logo.css';
 
 export default props => 
 <aside>
-    logo
+    <a href="/" className="logo">
+        <img src={logo} alt="logoLc" />
+    </a>
 </aside>
