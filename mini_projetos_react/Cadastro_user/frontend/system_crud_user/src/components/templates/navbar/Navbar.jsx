9b38cc95c548df450ebb72b76adcd5879b34a784
Lucas Felipe <lkzeron@gmail.com>
2023-08-15 00:30:27 -0300

    feat: estilização

--- mini_projetos_react/Cadastro_user/frontend/system_crud_user/src/components/templates/navbar/Navbar.jsx
+++ mini_projetos_react/Cadastro_user/frontend/system_crud_user/src/components/templates/navbar/Navbar.jsx
@@ -4,5 +4,13 @@ import React from 'react';
 
 export default props => 
   <aside className="menu-area">
-    navegação
+    {/* Refatora igual no header */}
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home"></i>Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i>Usários
+      </a>
+    </nav>
   </aside>
